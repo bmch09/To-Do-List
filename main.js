5936e0093a03fc986c6324dbc8cc1cb833bd7abc
bmch09 <bmch09@outlook.com>
2024-10-09 12:55:14 -0500

    Add Responsive and some mistakes

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const btnAdd = document.querySelector("#btn-add");
 const list = document.querySelector(".container_task");
 
 
+//*     Check the task
 
 function checkTask(){
     document.querySelector("#input-task").focus();
@@ -13,10 +14,31 @@ function checkTask(){
     }else{
         subTitulo.textContent = "Task to Do";
     }
+};
+
+function saveTasksToLocalStorage() {
+    const tasks = [];   //creamos array
+    document.querySelectorAll(".task").forEach(taskElement => { // seleccionamos elementos task, recorremos cada una 
+        tasks.push(taskElement.textContent);                //  obtenemos el contenido y lo guardamos en el array
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks)); // convertimos a string para guardar tareas 
+    console.log(tasks)
 }
 
-checkTask();
 
+function loadTasksFromLocalStorage() { // Cargar las tareas desde localStorage al cargar la página
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")); // convertimos a array para obtener las tareas guardadas
+    if (storedTasks) { //   validamos si hay tareas guardadas
+        storedTasks.forEach(task => { //    recorremos las tareas 
+            newTask(task); // Llamamos a newTask para agregar cada tarea al DOM
+        });
+    }
+    console.log(storedTasks)
+}
+
+
+
+//*     New task
 
 function newTask(task){
     let div = document.createElement("div");
@@ -41,15 +63,18 @@ function newTask(task){
     div.append(btnDoit,p,btnDelete,btnEdit)
     list.append(div);
 
+    //*     Eventos CRUD
 
     btnDelete.addEventListener("click",() => {
         div.remove();
+        saveTasksToLocalStorage();
         checkTask();
     });
 
     btnEdit.addEventListener("click", () => {
         const newTask = prompt("Editar tarea: ", p.textContent);
         p.textContent = newTask;
+        saveTasksToLocalStorage();
     });
 
     btnDoit.addEventListener("click", () => {
@@ -59,10 +84,10 @@ function newTask(task){
             p.classList.add("doit")
         }
     })
-
 }
 
 
+//*     Add task and Call function and change the subtittle 
 
 btnAdd.addEventListener("click", () => { 
 
@@ -71,15 +96,14 @@ btnAdd.addEventListener("click", () => {
     if(task === ""){
         alert("You should type a task")
     }else{
-
         newTask(task);
         document.querySelector("#input-task").value = ""
-    
         checkTask();
-    
+        saveTasksToLocalStorage();
     }
 
     
 });
 
-
+loadTasksFromLocalStorage()
+checkTask();
